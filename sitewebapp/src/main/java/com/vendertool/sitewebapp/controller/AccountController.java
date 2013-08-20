62f95e3936d0dae31481d4fc19fa4805948ff2b6
ted <ted@homer.com>
2013-08-19 22:25:23 -0700

    removing .classpath and .settings

--- sitewebapp/src/main/java/com/vendertool/sitewebapp/controller/AccountController.java
+++ sitewebapp/src/main/java/com/vendertool/sitewebapp/controller/AccountController.java
@@ -1,6 +1,7 @@
 package com.vendertool.sitewebapp.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.log4j.Logger;
 import org.codehaus.jackson.map.ObjectMapper;
@@ -11,11 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.vendertool.sharedtypes.core.Account;
+import com.vendertool.sharedtypes.core.Address;
+import com.vendertool.sharedtypes.core.ContactDetails;
+import com.vendertool.sharedtypes.core.CountryEnum;
 import com.vendertool.sitewebapp.common.FieldEnum;
+import com.vendertool.sitewebapp.model.AccountFieldBuilder;
 import com.vendertool.sitewebapp.model.Error;
 import com.vendertool.sitewebapp.model.Field;
 import com.vendertool.sitewebapp.model.Page;
-import com.vendertool.sitewebapp.test.util.MockDataUtil;
+import com.vendertool.sitewebapp.common.FieldEnum;
 
 @Controller
 public class AccountController {
@@ -26,7 +32,33 @@ public class AccountController {
 	public String getAccountPage(ModelMap modelMap) {
 		logger.info("account GET controller invoked");
 		
-		Page page = MockDataUtil.getAccountPage();
+
+		
+		Address address = new Address();
+		address.setAddressLine1("123 Main St");
+		address.setAddressLine2("Apt. B");
+		address.setCity("San Jose");
+		address.setState("CA");
+		address.setZip("95125");
+		address.setCountry(CountryEnum.UNITED_STATES);
+		
+		ContactDetails contact = new ContactDetails();
+		contact.setAddress(address);
+		contact.setFirstName("Ted");
+		contact.setLastName("Szeto");
+		
+		Account acct = new Account();
+		acct.setEmailId("ted@gmail.com");
+		acct.setContact(contact);
+
+		Map<FieldEnum, Field> fieldMap = AccountFieldBuilder.getFieldMap();
+		AccountFieldBuilder.setAccount(acct, fieldMap);
+		
+		Page page = new Page();
+		page.setFieldMap(fieldMap);
+		page.setUsername(acct.getEmailId());
+		
+
 
 		modelMap.put("page", page);
 		
